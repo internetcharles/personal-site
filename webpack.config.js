9f77072267925e669b537a116f77299aa2bffbc7
internetcharles <charlesodellsmith@gmail.com>
2020-12-12 14:14:54 -0800

    routing

--- webpack.config.js
+++ webpack.config.js
@@ -79,7 +79,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(jpeg|jpg|png|svg)$/,
+        test: /\.(jpeg|jpg|png|svg|ico|pdf)$/,
         use: {
           loader: 'url-loader',
           options: { limit: 1000 },
